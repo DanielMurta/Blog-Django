78ed4dc9ea0f86064edc508020cefdc01f429e99
Daniel Silveira <murtdaniel@gmail.com>
2023-01-04 19:35:20 -0300

    [Criando templates]

--- blog/settings.py
+++ blog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -138,6 +139,8 @@ MESSAGE_TAGS = {
         constants.SUCCESS: 'alert-success',
         constants.INFO: 'alert-info',
     }
+
+INSTALLED_APPS += ('django_summernote',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
+from django_summernote.admin import SummernoteModelAdmin
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('id', 'titulo_post', 'autor_post', 'data_post',
                     'categoria_post', 'publicado_post')
     list_editable = ('publicado_post',)
     list_display_links = ('id', 'titulo_post')
+    summernote_fields = ('conteudo_post',)
 
 admin.site.register(Post, PostAdmin)
 
