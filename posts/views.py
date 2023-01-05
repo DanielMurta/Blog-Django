ec078cb1b43747da76d599781d6f80de837fd503
Daniel Silveira <murtdaniel@gmail.com>
2023-01-05 18:25:55 -0300

    [Ordenando Posts][Implementando links e o mecanismo de busca]

--- posts/views.py
+++ posts/views.py
@@ -2,17 +2,48 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
     context_object_name = 'posts'
+    ordering = ['id']
+
 
 class PostBusca(PostIndex):
-    pass
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+
+        return qs
+
 
 class PostCategoria(PostIndex):
-    pass
+    template_name = 'posts/post_categoria.html'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
 class PostDetalhes(UpdateView):
     pass
